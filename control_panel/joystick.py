e25665670eecdbce763306c756139f2ffa2dbe9f
czogran <pawel.rawicki@gmail.com>
2021-11-14 09:30:07 -0800

    joystick example

---
+++ control_panel/joystick.py
@@ -0,0 +1,22 @@
+from rqt_gui_py.plugin import Plugin
+
+from .joystick1 import Joystick1
+
+
+class Joystick(Plugin):
+
+    def __init__(self, context):
+        super(Joystick, self).__init__(context)
+        self._node = context.node
+        self._logger = self._node.get_logger().get_child('rqt_rt_preempt.ros_rt_preempt.RosRtpreempt')
+
+        super(Joystick, self).__init__(context)
+        self.setObjectName('RosRtpreempt')
+
+        self._widget = Joystick1(context.node, self)
+
+        self._widget.start()
+        if context.serial_number() > 1:
+            self._widget.setWindowTitle(
+                self._widget.windowTitle() + (' (%d)' % context.serial_number()))
+        context.add_widget(self._widget)
